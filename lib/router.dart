e90b65bc05986450f9d45110f01527b02e039b0f
Koyu Fuke <kouyuu6@gmail.com>
2025-01-03 14:08:11 +0900

    refactor: router.dartの追加

---
+++ lib/router.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'views/views.dart';
+
+final goRouter = GoRouter(
+  initialLocation: '/',
+  routes: [],
+);
